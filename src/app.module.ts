af07a61978db497c5abfef728ceef0dce07569d6
Sergio <sfestatomari@gmail.com>
2024-02-11 12:43:11 -0300

    add QuoteModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { QuoteModule } from './quote/quote.module';
 import { GlobalModule } from './global/global.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     GlobalModule,
+    QuoteModule,
   ],
   controllers: [AppController],
   providers: [AppService],
